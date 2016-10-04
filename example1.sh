3d1608d25ea83a7730847ee20475316879e7a3be
tuetran <dark.seraphin.294@gmail.com>
2016-10-04 15:01:58 +0700

    second modified

--- example1.sh
+++ example1.sh
@@ -14,3 +14,16 @@ echo ${my_array[-1]}
 
 echo "Taking a slice of an array (like Python!)"
 echo ${my_array[*]:1:3}
+echo Reassigning index position 2
+my_array[2]='REASSIGNED!'
+echo 'Array position 2:' ${my_array[2]}
+
+cho Reassigning index position 2
+my_array[2]='REASSIGNED!'
+echo 'Array position 2:' ${my_array[2]}
+
+cho Reassigning index position 2
+my_array[2]='REASSIGNED!'
+echo 'Array position 2:' ${my_array[2]}
+
+
